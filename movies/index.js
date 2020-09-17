6997e703208e1e6b85832947d01df772bf9a33da
ozdemir-ahmet <ozdemir76@yahoo.com>
2020-09-17 23:12:45 +0200

    Just started the movie fight project

---
+++ movies/index.js
@@ -0,0 +1,19 @@
+const fetchData = async (searchTerm) => {
+   const response = await axios.get('http://www.omdbapi.com/', {
+       params : {
+           apikey: '79c9dd4d',
+           s: searchTerm
+       }
+   });
+
+   console.log(response.data)
+};
+
+const input = document.querySelector ('input');
+
+const onInput = (event => {
+    fetchData(event.target.value);
+});
+
+input.addEventListener('input', debounce (onInput,500));
+
