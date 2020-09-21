115ec05abde92a525f35f341773040f792dd095a
ozdemir-ahmet <ozdemir76@yahoo.com>
2020-09-22 00:42:37 +0200

    Further implementations done such as getting the movies from the api and showing them

--- movies/index.js
+++ movies/index.js
@@ -1,19 +1,50 @@
-const fetchData = async (searchTerm) => {
-   const response = await axios.get('http://www.omdbapi.com/', {
-       params : {
-           apikey: '79c9dd4d',
-           s: searchTerm
-       }
-   });
+const fetchData = async searchTerm => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'd9835cc5',
+        s: searchTerm
+      }
+    });
+  
+    if (response.data.Error) {
+      return [];
+    }
+  
+    return response.data.Search;
+  };
+  
+  const root = document.querySelector('.autocomplete');
+  root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+    </div>
+  `;
+  
+  const input = document.querySelector('input');
+  const dropdown = document.querySelector('.dropdown');
+  const resultsWrapper = document.querySelector('.results');
+  
+  const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+  
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
+    for (let movie of movies) {
+      const option = document.createElement('a');
+      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
-   console.log(response.data)
-};
-
-const input = document.querySelector ('input');
-
-const onInput = (event => {
-    fetchData(event.target.value);
-});
-
-input.addEventListener('input', debounce (onInput,500));
-
+      option.classList.add('dropdown-item');
+      option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
+      `;
+  
+      resultsWrapper.appendChild(option);
+    }
+  };
+  input.addEventListener('input', debounce(onInput, 500));
+  
